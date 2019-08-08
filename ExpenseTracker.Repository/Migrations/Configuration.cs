8af5cc792cb73af903150cf5daca782447959a35
onyekachila <onyekachila@gmail.com>
2019-08-08 06:57:50 +0100

    initial commit with repository controllers, migration, dto

---
+++ ExpenseTracker.Repository/Migrations/Configuration.cs
@@ -0,0 +1,23 @@
+namespace ExpenseTracker.Repository.Migrations
+{
+    using System;
+    using System.Data.Entity;
+    using System.Data.Entity.Migrations;
+    using System.Linq;
+
+    internal sealed class Configuration : DbMigrationsConfiguration<ExpenseTracker.Repository.Entities.ExpenseTrackerContext>
+    {
+        public Configuration()
+        {
+            AutomaticMigrationsEnabled = false;
+        }
+
+        protected override void Seed(ExpenseTracker.Repository.Entities.ExpenseTrackerContext context)
+        {
+            //  This method will be called after migrating to the latest version.
+
+            //  You can use the DbSet<T>.AddOrUpdate() helper extension method 
+            //  to avoid creating duplicate seed data.
+        }
+    }
+}
